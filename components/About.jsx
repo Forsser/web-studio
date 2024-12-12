4381cfe36dbd8084b46dd26e3e322069f160e1c8
Denys Yurkevych <dyushes007@gmail.com>
2024-12-12 18:32:41 +0200

    specialization section start

--- components/About.jsx
+++ components/About.jsx
@@ -61,7 +61,6 @@ export default function About() {
             </div>
           </li>
         </ul>
-        <Specialization />
       </div>
     </section>
   );
--- components/Specialization.jsx
+++ components/Specialization.jsx
@@ -1,12 +1,73 @@
 import style from "@/src/style/specialization.module.scss";
+import landing from "@/src/landing.jpg";
+import portfolio from "@/src/portfolio.jpg";
 
 export default function Specialization() {
   return (
     <div className={style.specialization}>
-      <div className={style.container}>
+      <div className="container">
         <h3 className={style.specialization__title}>Ми спеціалізуємось</h3>
 
-        <div className={style.specialization__container}></div>
+        <div className={style.specialization__container}>
+          <ul className={style.specialization__list}>
+            <li className={`${style.specialization__item} ${style.item1}`}>
+              <h4>Сайт візитка</h4>
+              <div className={style.item__container}>
+                <p className={style.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Eaque natus commodi recusandae rem aperiam corporis incidunt,
+                  obcaecati rerum, nobis quibusdam minima, voluptatibus
+                  accusamus aspernatur iste fuga velit facilis enim aut?
+                </p>
+                <div className={style.item__img}>
+                  <img className="" src={landing.src} />
+                </div>
+              </div>
+            </li>
+            <li className={`${style.specialization__item} ${style.item2}`}>
+              <h4>Сайт портфоліо</h4>
+              <div className={style.item__container}>
+                <div className={style.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Eaque natus commodi recusandae rem aperiam corporis incidunt,
+                  obcaecati rerum, nobis quibusdam minima, voluptatibus
+                  accusamus aspernatur iste fuga velit facilis enim aut?
+                </div>
+                <div className={style.item__img}>
+                  <img className="" src={portfolio.src} />
+                </div>
+              </div>
+            </li>
+            <li className={`${style.specialization__item} ${style.item3}`}>
+              <h4>Сайт візитка</h4>
+              <div className={style.item__container}>
+                <div className={style.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Eaque natus commodi recusandae rem aperiam corporis incidunt,
+                  obcaecati rerum, nobis quibusdam minima, voluptatibus
+                  accusamus aspernatur iste fuga velit facilis enim aut?
+                </div>
+                <div className={style.item__img}>
+                  <img className="" src={portfolio.src} />
+                </div>
+              </div>
+            </li>
+            <li className={`${style.specialization__item} ${style.item4}`}>
+              <h4>Сайт візитка</h4>
+              <div className={style.item__container}>
+                <div className={style.item__text}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Eaque natus commodi recusandae rem aperiam corporis incidunt,
+                  obcaecati rerum, nobis quibusdam minima, voluptatibus
+                  accusamus aspernatur iste fuga velit facilis enim aut?
+                </div>
+                <div className={style.item__img}>
+                  <img className="" src={portfolio.src} />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
