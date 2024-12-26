6f6eb95893247b10cb2eb30486c8118005ef058a
Denys Yurkevych <dyushes007@gmail.com>
2024-12-26 14:24:09 +0200

    adaptiv + cms data from hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,18 +1,19 @@
 import style from "../src/style/hero.module.scss";
 import ParticleAnimation from "@/components/ParticleAnimation";
 import Smile from "@/public/icons/smile.svg";
-import { toast } from "sonner";
+import { getHeroData } from "@/lib/api/hero";
 
-export default function Hero() {
+export default async function Hero() {
+  const { data } = await getHeroData();
+
+  const heroContent = data[0].hero;
   return (
     <section className={style.hero}>
       <div className="container">
         <div className={style.hero__title}>
-          <h1 className={style.hero__text}>
-            Ідеї, які <br /> реалізуються в коді{" "}
-          </h1>
+          <h1 className={style.hero__text}>{heroContent.title}</h1>
           <h2 className={style.hero__subtext}>
-            Технології для росту твоєї компанії
+            {heroContent.subtitle}
             <Smile alt="Smile" width={40} height={40} />
           </h2>
         </div>
