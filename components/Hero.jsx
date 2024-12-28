673ef3e07979b476a994487ed3643bf243fe682e
Denys Yurkevych <dyushes007@gmail.com>
2024-12-28 16:00:49 +0200

    cms off

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,16 +4,16 @@ import Smile from "@/public/icons/smile.svg";
 import { getHeroData } from "@/lib/api/hero";
 
 export default async function Hero() {
-  const { data } = await getHeroData();
+  /*   const { data } = await getHeroData(); */
 
-  const heroContent = data[0].hero;
+  /*   const heroContent = data[0].hero; */
   return (
     <section className={style.hero}>
       <div className="container">
         <div className={style.hero__title}>
-          <h1 className={style.hero__text}>{heroContent.title}</h1>
+          <h1 className={style.hero__text}>Ідеї, які реалізуються в коді</h1>
           <h2 className={style.hero__subtext}>
-            {heroContent.subtitle}
+            Технології, для росту твоєї компанії
             <Smile alt="Smile" width={40} height={40} />
           </h2>
         </div>
