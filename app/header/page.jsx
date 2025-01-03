edf02b151624e6add2dd8346d794e538a7647dd5
Denys Yurkevych <dyushes007@gmail.com>
2025-01-03 17:06:45 +0200

    other css

--- app/header/page.jsx
+++ app/header/page.jsx
@@ -10,45 +10,49 @@ export default function Header() {
         <div className={styles.header__container}>
           <div className={`${styles.header__logo} ${styles.logo}`}>Logo</div>
 
-          {/* Бургер меню для мобільних */}
-          <div className={styles.header__burger}>
-            <BurgerMenu />
-          </div>
+          <div className={styles.navigation}>
+            {/* Бургер меню для мобільних */}
+            <div className={styles.header__burger}>
+              <BurgerMenu />
+            </div>
 
-          {/* Десктопна навігація */}
-          <nav className={`${styles.header__navigation} ${styles.navigation}`}>
-            <ul className={styles.navigation__list}>
-              <li className={styles.navigation__item}>
-                <Link href="#example" className={styles.navigation__link}>
-                  Портфоліо
-                </Link>
-              </li>
-              <li className={styles.navigation__item}>
-                <Link
-                  href="#specialization"
-                  className={styles.navigation__link}
-                >
-                  Послуги
-                </Link>
-              </li>
-              <li className={styles.navigation__item}>
-                <Link href="#about" className={styles.navigation__link}>
-                  Про нас
-                </Link>
-              </li>
-              <li className={styles.navigation__item}>
-                <Link href="/portfolio" className={styles.navigation__link}>
-                  Контакти
-                </Link>
-              </li>
-            </ul>
-          </nav>
+            {/* Десктопна навігація */}
+            <nav
+              className={`${styles.header__navigation} ${styles.navigation}`}
+            >
+              <ul className={styles.navigation__list}>
+                <li className={styles.navigation__item}>
+                  <Link href="#example" className={styles.navigation__link}>
+                    Портфоліо
+                  </Link>
+                </li>
+                <li className={styles.navigation__item}>
+                  <Link
+                    href="#specialization"
+                    className={styles.navigation__link}
+                  >
+                    Послуги
+                  </Link>
+                </li>
+                <li className={styles.navigation__item}>
+                  <Link href="#about" className={styles.navigation__link}>
+                    Про нас
+                  </Link>
+                </li>
+                <li className={styles.navigation__item}>
+                  <Link href="/portfolio" className={styles.navigation__link}>
+                    Контакти
+                  </Link>
+                </li>
+              </ul>
+            </nav>
 
-          <div className={`${styles.header__contacts} ${styles.contacts}`}>
-            <div className={styles.contacts__info}>
-              <span className={styles.contacts__phone}>
-                +38 (000) 000-00-00
-              </span>
+            <div className={`${styles.header__contacts} ${styles.contacts}`}>
+              <div className={styles.contacts__info}>
+                <span className={styles.contacts__phone}>
+                  +38 (000) 000-00-00
+                </span>
+              </div>
             </div>
           </div>
         </div>
